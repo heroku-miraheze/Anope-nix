db8b980a47c7456f1606517c012f242303df0d85
heroku-miraheze <61781343+heroku-miraheze@users.noreply.github.com>
2021-05-21 15:07:10 +0200

    Create default.nix

---
+++ default.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {}}:
+let
+
+  bash libmysqlclient autoconf make gcc;
+  version = 2.0.9;
+  serviceName = "anope";
+  componentName = "${serviceName}";
+
+in
+{
+  "${componentName}" = stdenv.mkDerivation {
+    name = anope;
+    version = 2.0.9;
+
+    buildInputs = [ bash libmysqlclient autoconf make gcc ];
+    src = fetchurl {
+      url = "https://github.com/anope/anope/archive/refs/tags/2.0.9.tar.gz";
+      sha256 = d098ac3cd273c7a243fb1e32ce7aa702f09a6ca3d821a4544f72ef4d1f29ec36;
+    };
+    buildCommand = ''
+      ./configure
+      make
+      make install
+    '';
+  };
+}
